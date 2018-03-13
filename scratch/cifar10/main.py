98435e099502aac8360665696dd9bef41e7f3e4d
John Emmons <emmons.john@gmail.com>
2018-03-13 16:03:51 -0700

    resnet.py: add autoencoder (bottleneck) into the model main.py: updated the model that is being trained to be the autoencoder one

--- scratch/cifar10/main.py
+++ scratch/cifar10/main.py
@@ -1,7 +1,11 @@
 import argparse
 import h5py
+import os
+import json
+import shutil
 import logging
 import resnet
+import time
 import timeit
 
 import torch
@@ -10,7 +14,7 @@ import torch.optim as optim
 
 import numpy as np
 
-NUM_EPOCHS = 1000
+NUM_EPOCHS = 100
 logging.basicConfig(level=logging.DEBUG)
 
 
@@ -27,6 +31,9 @@ def train(model, loss_fn, optimizer, batch_size, _data, _data_labels):
     data = _data[p]
     data_labels = _data_labels[p]
 
+
+    train_loss = 0
+    correct = 0
     t1 = timeit.default_timer()
     for i in range(0, data_len, batch_size):
 
@@ -38,6 +45,11 @@ def train(model, loss_fn, optimizer, batch_size, _data, _data_labels):
         optimizer.zero_grad()
         output = model(batch_data)
         loss = loss_fn(output, batch_labels)
+
+        train_loss += loss.data[0]
+        pred = output.data.max(1, keepdim=True)[1]
+        correct += pred.eq(batch_labels.data.view_as(pred)).long().sum()
+
         loss.backward()
         optimizer.step()
 
@@ -48,6 +60,11 @@ def train(model, loss_fn, optimizer, batch_size, _data, _data_labels):
 
     t2 = timeit.default_timer()
     logging.info('Train Epoch took {} seconds'.format(t2-t1))
+
+    return {
+        'train_top1' : correct / data_len,
+        'train_loss' : train_loss
+        }
     
     
 def test(model, loss_fn, batch_size, data, data_labels):
@@ -78,9 +95,21 @@ def test(model, loss_fn, batch_size, data, data_labels):
 
     t2 = timeit.default_timer()
     logging.info('Test Epoch took {} seconds'.format(t2-t1))
-        
+
+    return {
+        'validation_top1' : correct / data_len,
+        'validation_loss' : test_loss
+        }
+
+    
 def main(args):
 
+    if os.path.isdir(args.checkpoint_dir):
+        logging.error('checkpoint dir already exists: {}'.format(args.checkpoint_dir))
+        return
+
+    os.makedirs(args.checkpoint_dir)
+    
     logging.info('loading data into memory')
     with h5py.File(args.data_hdf5, 'r') as f:
         train_data_raw = np.asarray(f['train_data_raw'])
@@ -89,31 +118,71 @@ def main(args):
         test_data_labels = np.asarray(f['test_data_labels'])
     logging.info('done! (loading data into memory)')
 
+    shutil.copy(args.data_hdf5, args.checkpoint_dir)
+
+    metadata = {
+        'creation_time' : time.strftime("%Y-%m-%d-%H:%M:%S", time.gmtime()),
+        'data_hdf5' : os.path.abspath(args.data_hdf5),
+        'checkpoint_dir' : os.path.abspath(args.checkpoint_dir),
+        'batch_size' : args.batch_size,
+        'compaction_factor' : args.compaction_factor,
+        'learning_rate' : args.lr,
+        'training_epochs' : NUM_EPOCHS, 
+        }
     
-    net = torch.nn.DataParallel(resnet.ResNet18())
+    with open(os.path.join(args.checkpoint_dir, 'metadata.json'), 'w') as f:
+        f.write(json.dumps(metadata, indent=4, sort_keys=True))
+    
+    net = torch.nn.DataParallel(resnet.AutoencoderResNet(compaction_factor=args.compaction_factor))
     net.cuda()
 
     optimizer = optim.Adam(net.parameters(), lr=args.lr)
+    #optimizer = optim.Adagrad(net.parameters(), lr=args.lr, lr_decay=0.01)
+    #optimizer = optim.RMSprop(net.parameters(), lr=args.lr)
     loss_fn = torch.nn.CrossEntropyLoss()
-    
-    for epoch in range(1, NUM_EPOCHS+1):
 
-        logging.info('begin training epoch: {}'.format(epoch))
-        train(net, loss_fn, optimizer, args.batch_size, train_data_raw, train_data_labels)
-        
-        logging.info('begin testing epoch: {}'.format(epoch))
-        test(net, loss_fn, args.batch_size, test_data_raw, test_data_labels)
+    with open(os.path.join(args.checkpoint_dir, 'training_log.csv'), 'w') as logfile:
 
-        # save model params here
+        logfile.write('# {\n')
+        for k in metadata.keys():
+            logfile.write('#     "{}": "{}",\n'.format(k, metadata[k]))
+        logfile.write('# }\n')
         
+        logfile.write('epoch, train_acc_top1, train_loss, validation_acc_top1, validation_loss, model_checkpoint\n') 
+        logfile.flush()
         
+        for epoch in range(1, NUM_EPOCHS+1):
 
+            logging.info('begin training epoch: {}'.format(epoch))
+            train_log = train(net, loss_fn, optimizer, args.batch_size, train_data_raw, train_data_labels)
+
+            logging.info('begin testing epoch: {}'.format(epoch))
+            test_log = test(net, loss_fn, args.batch_size, test_data_raw, test_data_labels)
+
+            if epoch % 5 == 0 or epoch in [1,2,3,4,5]:
+                checkpoint_filename = os.path.abspath(os.path.join(args.checkpoint_dir,
+                                                   'checkpoint-epoch{}.h5'.format(str(epoch).zfill(4))))
+                model_params = net.state_dict()
+                with h5py.File(checkpoint_filename, 'w') as f:
+                    for param_name in model_params.keys():
+                        f.create_dataset(param_name, data=model_params[param_name])
+
+                logfile.write('{},{},{},{},{},{}\n'.format(epoch,
+                                                           train_log['train_top1'],
+                                                           train_log['train_loss'],
+                                                           test_log['validation_top1'],
+                                                           test_log['validation_loss'],
+                                                           checkpoint_filename))
+                logfile.flush()
+
+                    
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('data_hdf5', type=str)
-    parser.add_argument('temp_dir', type=str)
+    parser.add_argument('checkpoint_dir', type=str)
 
+    parser.add_argument('--compaction_factor', type=float, default=1.0)
     parser.add_argument('--lr', type=float, default=0.001, 
                         help='learning rate (default: 0.001)')
     parser.add_argument('--batch_size', type=int, default=1000,
--- scratch/cifar10/resnet.py
+++ scratch/cifar10/resnet.py
@@ -66,15 +66,84 @@ class Bottleneck(nn.Module):
         return out
 
 
-class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
-        super(ResNet, self).__init__()
+class Autoencoder(nn.Module):
+
+    def __init__(self, num_planes_input, activation_compaction_factor):
+        super(Autoencoder, self).__init__()
+
+        num_planes_layer1 = int(num_planes_input // activation_compaction_factor)
+        num_planes_layer2 = int(num_planes_layer1 // activation_compaction_factor)
+        num_planes_layer3 = int(num_planes_layer2 // activation_compaction_factor)
+        
+        self.encoder = nn.Sequential(
+            nn.Conv2d(num_planes_input, num_planes_input, 1, stride=1, padding=0),
+            nn.BatchNorm2d(num_planes_input),
+            nn.ReLU(True),
+            nn.Conv2d(num_planes_input, num_planes_layer1, 3, stride=1, padding=1),
+            nn.BatchNorm2d(num_planes_layer1),
+            nn.ReLU(True),
+            #
+            nn.Conv2d(num_planes_layer1, num_planes_layer1, 1, stride=1, padding=0),
+            nn.BatchNorm2d(num_planes_layer1),
+            nn.ReLU(True),
+            nn.Conv2d(num_planes_layer1, num_planes_layer2, 3, stride=1, padding=1),
+            nn.BatchNorm2d(num_planes_layer2),
+            nn.ReLU(True),
+            #
+            nn.Conv2d(num_planes_layer2, num_planes_layer2, 1, stride=1, padding=0),
+            nn.BatchNorm2d(num_planes_layer2),
+            nn.ReLU(True),
+            nn.Conv2d(num_planes_layer2, num_planes_layer3, 3, stride=1, padding=1),
+            nn.BatchNorm2d(num_planes_layer3),
+            nn.ReLU(True),
+            )
+
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(num_planes_layer3, num_planes_layer3, 1, stride=1, padding=0),
+            nn.BatchNorm2d(num_planes_layer3),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(num_planes_layer3, num_planes_layer2, 3, stride=1, padding=1),
+            nn.BatchNorm2d(num_planes_layer2),
+            nn.ReLU(True),
+            #
+            nn.ConvTranspose2d(num_planes_layer2, num_planes_layer2, 1, stride=1, padding=0),
+            nn.BatchNorm2d(num_planes_layer2),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(num_planes_layer2, num_planes_layer1, 3, stride=1, padding=1),
+            nn.BatchNorm2d(num_planes_layer1),
+            nn.ReLU(True),
+            #
+            nn.ConvTranspose2d(num_planes_layer1, num_planes_layer1, 1, stride=1, padding=0),
+            nn.BatchNorm2d(num_planes_layer1),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(num_planes_layer1, num_planes_input, 3, stride=1, padding=1),
+            nn.BatchNorm2d(num_planes_input),
+            nn.ReLU(True),
+            )
+        
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+    
+class AutoencoderResNet(nn.Module):
+    '''
+    Default configuration is a resnet18-like model with an autoencoder
+    after the conv_3 layer (see table 1 in https://arxiv.org/pdf/1512.03385.pdf).
+    '''
+    
+    def __init__(self, compaction_factor=1, block=BasicBlock, num_blocks=[2,2,2,2], num_classes=10):
+        super(AutoencoderResNet, self).__init__()
         self.in_planes = 64
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=1, padding=3, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+
+        self.autoencoder = Autoencoder(128, compaction_factor)
+
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
         self.linear = nn.Linear(512*block.expansion, num_classes)
@@ -91,6 +160,44 @@ class ResNet(nn.Module):
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.layer1(out)
         out = self.layer2(out)
+        out = self.autoencoder(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+    
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+
+        self.autoencoder = Autoencoder(128, 2)
+
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.autoencoder(out)
         out = self.layer3(out)
         out = self.layer4(out)
         out = F.avg_pool2d(out, 4)
