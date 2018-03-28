68aabc0d167fc9bc6193c7b1043bb0c1bbf3a38f
John Emmons <emmons.john@gmail.com>
2018-03-27 17:24:54 -0700

    made the noop code a little less trivial

--- src/frontend/main.cc
+++ src/frontend/main.cc
@@ -1,8 +1,16 @@
 #include <iostream>
 
+#include "noop.hh"
+
 int main(int argc, char* argv[]){
 
     std::cout << argc << " " << argv[0] << "\n";
     std::cout << "hello world\n";
+
+    Noop n;
+
+    std::cout << "42 == " << n.encode(42) << "\n";
+    std::cout << "777 == " << n.decode(777) << "\n";
+    
     return 0;
 }
--- src/modules/noop.cc
+++ src/modules/noop.cc
@@ -1,1 +1,9 @@
 #include "noop.hh"
+
+Noop::Noop() {}
+
+Noop::~Noop() {}
+
+int Noop::encode(int x) { return x; }
+
+int Noop::decode(int x) { return x; }
