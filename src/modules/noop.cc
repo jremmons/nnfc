91a5a86190cc26c9dec58a1fa2e59a1df990ab9a
John Emmons <emmons.john@gmail.com>
2018-03-29 14:39:53 -0700

    added separate encoder and decoder function

--- src/modules/noop.cc
+++ src/modules/noop.cc
@@ -10,16 +10,25 @@ Noop::Noop() {}
 Noop::~Noop() {}
 
 void Noop::encode(Blob3D<float> &input, Blob1D<uint8_t> &output) {
-    input.set(0,0,0,0);
-
-    size_t size = 256;
-    output.resize(size);
-    for(size_t i = 0; i < size; i++){
-        output.set(i, i);
+    output.resize(3*100*100);
+        
+    for(size_t i = 0; i < 3; i++){
+        for(size_t j = 0; j < 100; j++){
+            for(size_t k = 0; k < 100; k++){
+                output.set(input.get(i,j,k), 100*100*i + 100*j + k);
+            }
+        }
     }
 }
 
 void Noop::decode(Blob1D<uint8_t> &input, Blob3D<float> &output) {
-    input.set(0,0);
-    output.set(0,0,0,0);
+    output.resize(3, 100, 100);
+        
+    for(size_t i = 0; i < 3; i++){
+        for(size_t j = 0; j < 100; j++){
+            for(size_t k = 0; k < 100; k++){
+                output.set(input.get(100*100*i + 100*j + k), i, j, k);
+            }
+        }
+    }
 }
