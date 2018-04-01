ffa8a5c2f623a8a710d09d0e032cc31187acc26c
John Emmons <emmons.john@gmail.com>
2018-04-01 15:23:12 -0700

    continued to add stubs for new nnfc code

---
+++ src/modules/nnfc.hh
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <cstdint>
+
+#include "blob1d.hh"
+#include "blob4d.hh"
+
+namespace NNFC {
+
+    void encode(Blob4D<float> &input, Blob1D<uint8_t> &output);
+    void decode(Blob1D<uint8_t> &input, Blob4D<float> &output);
+
+}
