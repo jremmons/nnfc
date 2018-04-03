6b602ae2b962a3f32687df8965ed562235596b38
John Emmons <emmons.john@gmail.com>
2018-04-02 19:09:44 -0700

    updated travis for python test

--- python/install.sh
+++ python/install.sh
@@ -2,6 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+cd $DIR
 python setup.py clean --all
 python setup.py bdist_wheel
-pip install --force-reinstall --upgrade $DIR/dist/*.whl
+pip install --force-reinstall --upgrade dist/*.whl
