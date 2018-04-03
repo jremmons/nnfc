8761e521968e345eafcfb72fd98e729dc3f8db46
John Emmons <emmons.john@gmail.com>
2018-04-02 19:58:53 -0700

    added explicit use of python3

--- python/install.sh
+++ python/install.sh
@@ -3,6 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR
-python setup.py clean --all
-python setup.py bdist_wheel
-pip install --force-reinstall --upgrade dist/*.whl
+python3 setup.py clean --all
+python3 setup.py bdist_wheel
+pip3 install --force-reinstall --upgrade dist/*.whl
