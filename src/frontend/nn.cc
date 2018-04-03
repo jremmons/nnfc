e3b2b7a5fccc464ee2a15d8088cc43b3eb9e4222
John Emmons <emmons.john@gmail.com>
2018-04-02 18:20:37 -0700

    added stubs for C++ implementation of nn

---
+++ src/frontend/nn.cc
@@ -0,0 +1,11 @@
+#include "nn.hh"
+
+#include "blob1d.hh"
+#include "blob4d.hh"
+
+
+void NN::forward(Blob4D<float>&, Blob1D<uint64_t>&) {
+
+    // todo perform forward pass and store predictions into labels
+    
+}
