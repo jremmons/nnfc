a56d59a6081238df94a0314313b73ccee181a3eb
John Emmons <emmons.john@gmail.com>
2018-04-02 20:23:14 -0700

    remove --plat-name

--- python/install.sh
+++ python/install.sh
@@ -4,5 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR
 python3 setup.py clean --all
-python3 setup.py --plat-tag linux_x86-64 bdist_wheel
+python3 setup.py bdist_wheel
 pip3 install --force-reinstall --upgrade dist/*.whl
