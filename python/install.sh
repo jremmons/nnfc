f7342c266ed1dacb3b6e16975e8e61d83de99e32
John Emmons <emmons.john@gmail.com>
2018-04-07 16:05:11 -0700

    removed the requirement to `make install` to run tests

--- python/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-cd $DIR
-python3 setup.py clean --all
-python3 setup.py bdist_wheel
-pip3 install --force-reinstall --upgrade dist/*.whl
