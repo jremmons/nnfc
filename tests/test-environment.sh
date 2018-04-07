034a3f1f195d0c107e24b9963508e7b25cfd6efa
John Emmons <emmons.john@gmail.com>
2018-04-07 16:24:07 -0700

    tests/test-environment.sh: will add the correct libdir to PYTHONPATH

--- tests/test-environment.sh
+++ tests/test-environment.sh
@@ -1,4 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-export PYTHONPATH=../python/build/lib.linux-x86_64-3.6:$PYTHONPATH
+lib_count=$(ls ../python/build/ | grep 'lib' | wc -l)
+if [[ $lib_count != '1' ]]; then
+    echo -e '\n\n'
+    echo "ERROR: too many python lib dirs! (count: $lib_count) (ls: $(ls ../python/build/))"
+    echo -e '\n\n'
+    exit -1
+fi
+
+libdir=$(ls ../python/build/ | grep 'lib')
+
+export PYTHONPATH=$libdir:$PYTHONPATH
 exec "$@"
