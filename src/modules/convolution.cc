eabc6b02d6b150e2b10a8814a238b4db1532d33d
John Emmons <emmons.john@gmail.com>
2018-04-09 15:09:01 -0700

    updated convolution class to accept blob4D inputs

--- src/modules/convolution.cc
+++ src/modules/convolution.cc
@@ -1,13 +1,12 @@
 #include "convolution.hh"
 
 #include "blob1d.hh"
+#include "blob3d.hh"
 #include "blob4d.hh"
 
-void NN::conv2d(const Blob4D<float> &input, const Blob3D<float> &kernel,
-                Blob4D<uint64_t> &output,
-                const size_t stride = 0,
-                const size_t zero_padding = 0) {
+// void NN::conv2d(const Blob4D<float> &input, const Blob3D<float> &kernel,
+//                 Blob4D<uint64_t> &output,
+//                 const size_t stride = 0,
+//                 const size_t zero_padding = 0) {
     
-    
-    
-}
+// }
