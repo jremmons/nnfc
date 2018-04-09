efa98ef3d31f67578fcb29f8bb68364043abe63d
John Emmons <emmons.john@gmail.com>
2018-04-09 16:03:50 -0700

    wrote test for relu activation function

--- tests/relu_test.cc
+++ tests/relu_test.cc
@@ -1,8 +1,62 @@
+#include <H5Cpp.h>
 #include <iostream>
+
+#include <memory>
+
+#include "blob4d.hh"
+#include "activation.hh"
 
 int main(int argc, char* argv[]){
 
-    std::cout << argc << " " << argv[1] << "\n";
+    if(argc != 2){
+        throw std::runtime_error(std::string("usage: ") + argv[1]  + "<input h5file>");
+    }
+    
+    H5::H5File test_file(argv[1], H5F_ACC_RDONLY);
+
+    H5::DataSet input = test_file.openDataSet("input");
+    H5::DataSet output = test_file.openDataSet("output");
+
+    size_t input_ndims = input.getSpace().getSimpleExtentNdims();
+    size_t output_ndims = output.getSpace().getSimpleExtentNdims();
+
+    assert(input_ndims == output_ndims);
+    assert(input_ndims == 4);
+
+    hsize_t input_dims[4];
+    hsize_t output_dims[4];
+    input.getSpace().getSimpleExtentDims(input_dims, NULL);
+    output.getSpace().getSimpleExtentDims(output_dims, NULL);
+
+    size_t input_size = input_dims[0] * input_dims[1] * input_dims[2] * input_dims[3];
+    size_t output_size = output_dims[0] * output_dims[1] * output_dims[2] * output_dims[3];
+    
+    std::unique_ptr<float> input_data(new float[input_size]);
+    std::unique_ptr<float> output_data(new float[output_size]);
+    std::unique_ptr<float> output_data_correct(new float[output_size]);
+
+    for(size_t i = 0; i < input_size; i++) {
+        output_data.get()[i] = i; 
+    }
+
+    // load the input and correct result
+    input.read(input_data.get(), H5::PredType::NATIVE_FLOAT);
+    output.read(output_data_correct.get(), H5::PredType::NATIVE_FLOAT);
+
+    Blob4D<float> input_blob{input_data.get(), input_dims[0], input_dims[1], input_dims[2], input_dims[3]};
+    Blob4D<float> output_blob{output_data.get(), output_dims[0], output_dims[1], output_dims[2], output_dims[3]};
+
+    Blob4D<float> output_blob_correct{output_data_correct.get(), output_dims[0], output_dims[1], output_dims[2], output_dims[3]};
+    
+    NN::relu(input_blob, output_blob); // run the actication function
+
+    // check output blob
+    for(size_t i = 0; i < input_size; i++) {
+        if(output_data.get()[i] != output_data_correct.get()[i]){
+            throw std::runtime_error("hihi");
+        }
+    }
+    
     return 0;
     
 }
