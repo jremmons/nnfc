f3b8facef5cb95a1beb4da601242bbcf1659532b
John Emmons <emmons.john@gmail.com>
2018-04-09 11:54:12 -0700

    added hdf5 libs and checks

--- tests/conv2d_test.cc
+++ tests/conv2d_test.cc
@@ -1,7 +1,13 @@
+#include <H5Cpp.h>
+
 #include <iostream>
 
 int main(int argc, char* argv[]){
 
+    H5::H5File file("/tmp/test.h5", H5F_ACC_TRUNC);
+
+    
+    
     std::cout << argc << " " << argv[1] << "\n";
     return 0;
     
