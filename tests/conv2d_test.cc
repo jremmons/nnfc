be42a4d86a15252f8534a69e94d83de0c1469fcf
John Emmons <emmons.john@gmail.com>
2018-04-09 12:23:52 -0700

    add nontrivial hdf5 stub to testing code

--- tests/conv2d_test.cc
+++ tests/conv2d_test.cc
@@ -1,14 +1,22 @@
 #include <H5Cpp.h>
 
 #include <iostream>
+#include <stdexcept>
 
 int main(int argc, char* argv[]){
 
-    H5::H5File file("/tmp/test.h5", H5F_ACC_TRUNC);
+    if(argc != 2){
+        throw std::runtime_error(std::string("usage: ") + argv[1]  + "<input h5file>");
+    }
+    
+    H5::H5File test_file(argv[1], H5F_ACC_RDONLY);
 
+    H5::DataSet input = test_file.openDataSet("input");
+    H5::DataSet kernel = test_file.openDataSet("kernel");
+    H5::DataSet stride = test_file.openDataSet("stride");
+    H5::DataSet padding = test_file.openDataSet("padding");
+    H5::DataSet output = test_file.openDataSet("output");
     
-    
-    std::cout << argc << " " << argv[1] << "\n";
     return 0;
     
 }
