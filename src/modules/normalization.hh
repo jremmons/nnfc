243df9d7c67acc62002b6ab03e8a711741d86149
John Emmons <emmons.john@gmail.com>
2018-04-11 21:23:26 -0700

    added bias and weight parameters to batch_normalization

--- src/modules/normalization.hh
+++ src/modules/normalization.hh
@@ -8,6 +8,8 @@ namespace NN {
     void batch_norm(const Blob4D<float> &input, 
                     const Blob1D<float> &means,
                     const Blob1D<float> &variances,
+                    const Blob1D<float> &weight,
+                    const Blob1D<float> &bias,
                     Blob4D<float> &output,
                     const float eps = 0.00001);
     
