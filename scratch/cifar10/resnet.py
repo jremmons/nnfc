8f1156f2d117c686837d4e52ca7087d0e855f4bd
John Emmons <emmons.john@gmail.com>
2018-04-20 21:53:01 -0700

    updated the training code

--- scratch/cifar10/resnet.py
+++ scratch/cifar10/resnet.py
@@ -12,25 +12,6 @@ import torch.nn.functional as F
 
 from torch.autograd import Variable
 
-class TinyNet2(nn.Module):
-    def __init__(self, num_classes=10):
-        super(TinyNet2, self).__init__()
-        self.in_planes = 64
-
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        
-        self.linear = nn.Linear(64, num_classes, bias=False)
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = F.avg_pool2d(out, 32)
-        out = out.view(out.size(0), -1)
-        out = self.linear(out)
-        return out
-
-def TinyNet():
-    return TinyNet2()
 
 class BasicBlock(nn.Module):
     expansion = 1
@@ -92,12 +73,10 @@ class ResNet(nn.Module):
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
-
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        
         self.linear = nn.Linear(512*block.expansion, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
@@ -110,16 +89,15 @@ class ResNet(nn.Module):
 
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
-
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
-
         out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
+
 
 def ResNet18():
     return ResNet(BasicBlock, [2,2,2,2])
