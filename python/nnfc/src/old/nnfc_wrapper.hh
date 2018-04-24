a049ac4665c47772fc24f7f98e36664e30982730
John Emmons <emmons.john@gmail.com>
2018-04-24 11:21:20 -0700

    removed old files

--- python/nnfc/src/old/nnfc_wrapper.hh
+++
@@ -1,6 +0,0 @@
-
-int nnfc_encode_forward(THFloatTensor *input, THByteTensor *output);
-int nnfc_encode_backward(THFloatTensor *grad_output, THFloatTensor *grad_input);
-
-int nnfc_decode_forward(THByteTensor *input, THFloatTensor *output);
-int nnfc_decode_backward(THFloatTensor *grad_output, THFloatTensor *grad_input);
