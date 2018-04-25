fce893a6c6370c5f14d6c3754fc957f45270dd2f
John Emmons <emmons.john@gmail.com>
2018-04-25 16:49:33 -0700

    added move constructor

--- src/modules/tensor.hh
+++ src/modules/tensor.hh
@@ -13,30 +13,38 @@ namespace NNFC {
     template <typename T, int ndims>
     class Tensor {
     private:
+        Eigen::DSizes<Eigen::Index, ndims> size_;
         std::unique_ptr<T, void(*)(T*)> data_;
         Eigen::TensorMap<Eigen::Tensor<T, ndims>> tensor_;
 
     public:
         template<typename... DimSizes>
         Tensor(T* data, DimSizes&&... dims) :
+            size_(dims...),
             data_(data, [](T*){}), 
-            tensor_(Eigen::TensorMap<Eigen::Tensor<T, ndims>>(data_.get(), Eigen::DSizes<Eigen::Index, ndims>(dims...)))
+            tensor_(Eigen::TensorMap<Eigen::Tensor<T, ndims>>(data_.get(), size_))
         {            
             // note: the deleter for the data_ member was set to a noop
             // because when you use this constructor data_ is not owned by
             // this object. 
         }
-        
+
         // TODO(jremmons) get rid of the "new" for memory allocation.
         // Should be replaced by something like std::make_unique.
         template<typename... DimSizes>
-        Tensor(DimSizes&&... dims) :
-            data_(new T[Eigen::DSizes<Eigen::Index, ndims>(dims...).TotalSize()], [](T* ptr){ delete ptr; }), 
-            tensor_(Eigen::TensorMap<Eigen::Tensor<T, ndims>>(data_.get(), Eigen::DSizes<Eigen::Index, ndims>(dims...)))
+        Tensor(DimSizes... dims) :
+            size_(dims...),
+            data_(new T[size_.TotalSize()], [](T* ptr){ delete ptr; }), 
+            tensor_(Eigen::TensorMap<Eigen::Tensor<T, ndims>>(data_.get(), size_))
+        { }
+            
+        Tensor(Tensor<T, ndims>&& other) noexcept :
+            size_(other.size_),
+            data_(std::move(other.data_)),
+            tensor_(Eigen::TensorMap<Eigen::Tensor<T, ndims>>(data_.get(), size_))
         { }
 
         Tensor(const Tensor<T, ndims>&) = delete;
-        Tensor(const Tensor<T, ndims>&&) = delete;
         ~Tensor() { }
 
         Tensor<T, ndims>& operator=(Tensor<T, ndims> &rhs) = delete;    
