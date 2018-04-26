f8db70dfccbbb3fefb0d18183ac4a9d28249fa10
John Emmons <emmons.john@gmail.com>
2018-04-26 16:23:03 -0700

    cleaned up the setup script considerably

--- python/setup.py
+++ python/setup.py
@@ -4,8 +4,9 @@ import re
 import torch
 import numpy
 
-from setuptools import setup, find_packages
-from torch.utils.cpp_extension import BuildExtension,  CppExtension, CUDAExtension
+from setuptools import setup, find_packages, Extension
+
+from torch.utils.cpp_extension import BuildExtension, include_paths, library_paths
 
 def get_def(header_filepath, definition_name):
     with open(header_filepath, 'r') as f:
@@ -18,48 +19,28 @@ VERSION = get_def('../config.h', 'VERSION')
 EXTENSION_NAME = 'nnfc._ext.nnfc_codec'
 CUDA_AVAILABLE = torch.cuda.is_available()
 
-'''
-Defined the extension module below
-'''
-base_sources=['nnfc/src/nnfc_codec.cc',
-              'nnfc/src/nnfc_encoder.cc', 'nnfc/src/nnfc_decoder.cc']
-base_define_macros=[('_NNFC_VERSION', VERSION)]
-base_include_dirs=[numpy.get_include(), '../src/modules', './extra_headers']
-base_library_dirs=['../src/modules/.libs']
-base_libraries=[]
-base_extra_compile_args=[]
-base_extra_link_args=['-lnnfc']
+pytorch_include = []
+for lib in include_paths(cuda=CUDA_AVAILABLE):
+    pytorch_include += ['-isystem', lib]
 
+pytorch_libdirs = library_paths(cuda=CUDA_AVAILABLE)
+pytorch_libs = ['cudart']
+pytorch_defines = [('_NNFC_CUDA_AVAILABLE', 1)] if CUDA_AVAILABLE else []
 
-module = None
-if CUDA_AVAILABLE:
-    print('CUDA is available! Compiling the additional CUDA extension code.')
+module = Extension(EXTENSION_NAME,
+                   sources=['nnfc/src/nnfc_codec.cc', 'nnfc/src/nnfc_cuda.cc', 
+                            'nnfc/src/nnfc_encoder.cc', 'nnfc/src/nnfc_decoder.cc'],
+                   define_macros=[('_NNFC_VERSION', VERSION)] + pytorch_defines,
+                   include_dirs=[numpy.get_include()],
+                   library_dirs=['../src/modules/.libs'] + pytorch_libdirs,
+                   libraries=[] + pytorch_libs,
+                   extra_compile_args=['-I../src/modules', '-isystem', './extra_headers'] + pytorch_include,
+                   extra_link_args=['-Wl,-Bdynamic', '-lnnfc']
+                   )
 
-    module = CUDAExtension(name=EXTENSION_NAME,
-                          sources=base_sources + ['nnfc/src/nnfc_cuda.cc'],
-                          define_macros=base_define_macros,
-                          include_dirs=base_include_dirs,
-                          library_dirs=base_library_dirs,
-                          libraries=base_libraries,
-                          extra_compile_args=base_extra_compile_args + ['-D=_NNFC_CUDA_AVAILABLE=1'],
-                          extra_link_args=base_extra_link_args
-    )
-
-else:
-    module = CppExtension(name=EXTENSION_NAME,
-                          sources=base_sources,
-                          define_macros=base_define_macros + [],
-                          include_dirs=base_include_dirs,
-                          library_dirs=base_library_dirs,
-                          libraries=base_libraries,
-                          extra_compile_args=base_extra_compile_args,
-                          extra_link_args=base_extra_link_args
-    )
-
-assert module is not None    
 setup(
-    name='nnfc_codec',
-    version='0.0.0',
+    name=EXTENSION_NAME,
+    version=VERSION,
     description='',
     long_description='',
     url='',
