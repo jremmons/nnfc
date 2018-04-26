3f2308baed46db3012cde9adb2529404da74cfad
John Emmons <emmons.john@gmail.com>
2018-04-26 00:13:43 -0700

    trying out the new tensor class

--- src/frontend/main.cc
+++ src/frontend/main.cc
@@ -21,15 +21,25 @@ int main(int argc, char* argv[]){
     std::cout << tensor.dimension(1) << std::endl;
     std::cout << tensor.dimension(2) << std::endl;
     std::cout << tensor.dimension(3) << std::endl;
+
+    std::cout << tensor.size() << std::endl;
+    std::cout << tensor.rank() << std::endl;    
+
+    std::cout << tensor(0,0,1,3) << std::endl;
     
     NNFC::Tensor<float, 2> new_tensor(data, 32, 32);
-    std::cout << new_tensor.dimension(0) << std::endl;
-    std::cout << new_tensor.dimension(1) << std::endl;
+    // std::cout << new_tensor.dimension(0) << std::endl;
+    // std::cout << new_tensor.dimension(1) << std::endl;
 
     NNFC::Tensor<float, 2> create_tensor(32, 32);
     std::cout << create_tensor.dimension(0) << std::endl;
     std::cout << create_tensor.dimension(1) << std::endl;
-    
+
+    auto move_tensor = std::move(create_tensor);
+    std::cout << move_tensor.dimension(0) << std::endl;
+    std::cout << move_tensor.dimension(1) << std::endl;
+
+    //auto copy_tensor = create_tensor;
     // forward pass of nn
     // todo add code here
     //Blob3D<float> kernel_weights{nullptr, 0, 0, 0};
