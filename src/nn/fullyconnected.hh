9eea363a5eb870f5ad8d40440d849cf0cf5663e9
John Emmons <emmons.john@gmail.com>
2018-04-30 11:12:11 -0700

    added C++ forward pass demo

--- src/nn/fullyconnected.hh
+++ src/nn/fullyconnected.hh
@@ -8,6 +8,11 @@ namespace nn {
     void fully_connected(const Tensor<float, 4> input,
                          const Tensor<float, 2> weights,
                          Tensor<float, 4> output);
+
+    void fully_connected_with_bias(const Tensor<float, 4> input,
+                                   const Tensor<float, 2> weights,
+                                   const Tensor<float, 1> bias,
+                                   Tensor<float, 4> output);
     
 }
 
