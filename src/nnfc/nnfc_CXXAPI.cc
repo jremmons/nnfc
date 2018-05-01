63f991b91aeb844c3a5b02f1a105ac137c53c027
John Emmons <emmons.john@gmail.com>
2018-05-01 16:33:11 -0700

    renamed API files

---
+++ src/nnfc/nnfc_CXXAPI.cc
@@ -0,0 +1,7 @@
+#include <vector>
+#include <string>
+
+#include "nnfc_CXXAPI.hh"
+
+using namespace nnfc::cxxapi;
+
---
+++ src/nnfc/nnfc_CXXAPI.hh
@@ -0,0 +1,19 @@
+#ifndef _NNFC_CXXAPI_H
+#define _NNFC_CXXAPI_H
+
+#include <vector>
+#include <string>
+
+#include "nnfc.hh"
+#include "tensor.hh"
+
+namespace nnfc { namespace cxxapi {
+        
+        // TODO(jremmons) eventually we will want to create a standardized way
+        // for external code projects (including our own python API) to
+        // interface with our code. This header file is my first attempt at
+        // outlining a possible API for external projects.         
+        
+    }
+}
+#endif // _NNFC_CXXAPI_H
