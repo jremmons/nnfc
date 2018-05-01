e711e77a1ee9298ecafe54dccf71f17ec1716dae
John Emmons <emmons.john@gmail.com>
2018-04-30 21:18:53 -0700

    tensor.hh: cleaned up the constructors and added a some comments to help plebs understand (just kidding, I love comments :])

--- src/nn/tensor.hh
+++ src/nn/tensor.hh
@@ -13,29 +13,11 @@ namespace nn {
 
     typedef Eigen::Index Index;
 
-    // template<typename T>
-    // class Rawptr {
-    // private:
-    //     std::shared_ptr<T> ptr_;
-    //     size_t size_;
-
-    // public:
-    //     rawptr(T* ptr, size_t size) :
-    //         ptr_(ptr),
-    //         size_(size)
-    //     { }
-
-    //     ~rawptr() { }        
-
-    //     std::shared_ptr<T> get() { return ptr_; }
-    //     size_t size() { return size_; }
-    // };
-    
     template <typename T, int ndims>
     class Tensor {
     private:
-        Eigen::DSizes<Eigen::Index, ndims> size_;
-        std::shared_ptr<T> data_;
+        const Eigen::DSizes<Eigen::Index, ndims> size_;
+        const std::shared_ptr<T> data_;
         Eigen::TensorMap<Eigen::Tensor<T, ndims, Eigen::RowMajor>> tensor_;
 
         T* data()
@@ -44,51 +26,52 @@ namespace nn {
         }
         
     public:
+        // Note: this constructor does not take ownership of the
+        // input `data`. As a result, the `data` pointer is
+        // wrapped in a shared_ptr with noop delete function. Take
+        // care to ensure the lifetime of the `data` pointer
+        // covers the usage of this tensor object.
+        //
+        // Recommendation: use a constructor with a shared_ptr
+        // input if you want the tensor to take ownership of data.
+        // You won't have to worry about the lifetime of the
+        // data and/or tensor object then!
         template<typename... DimSizes>
-        Tensor(T* data, DimSizes&&... dims) :
-            size_(dims...),
-            data_(data, [](T*){}), 
-            tensor_(Eigen::TensorMap<Eigen::Tensor<T, ndims, Eigen::RowMajor>>(data_.get(), size_))
-        {            
-            // note: the deleter for the data_ member was set to a noop
-            // because when you use this constructor data_ is not owned
-            // by this object. 
-        }
-
-        // TODO(jremmons) get rid of the "new" for memory allocation.
-        // Should be replaced by something like std::make_unique.
-        template<typename... DimSizes>
-        Tensor(DimSizes... dims) :
-            size_(dims...),
-            data_(new T[size_.TotalSize()], [](T* ptr){ delete ptr; }), 
-            tensor_(Eigen::TensorMap<Eigen::Tensor<T, ndims, Eigen::RowMajor>>(data_.get(), size_))
+        Tensor(T* data, const DimSizes... dims) :
+            Tensor(std::shared_ptr<T>(data, [](T*){}), Eigen::DSizes<Eigen::Index, ndims>(dims...))
         { }
 
-        // make this the real constructor for the object
-        Tensor(T* data, Eigen::DSizes<Eigen::Index, ndims> size) :
-            size_(size),
-            data_(data, [](T*){}),
-            tensor_(Eigen::TensorMap<Eigen::Tensor<T, ndims, Eigen::RowMajor>>(data_.get(), size_))
+        template<typename... DimSizes>
+        Tensor(std::shared_ptr<T> data, const DimSizes... dims) :
+            Tensor(data, Eigen::DSizes<Eigen::Index, ndims>(dims...))
         { }
-               
-        Tensor(Eigen::DSizes<Eigen::Index, ndims> size) :
-            size_(size),
-            data_(new T[size_.TotalSize()]),
-            tensor_(Eigen::TensorMap<Eigen::Tensor<T, ndims, Eigen::RowMajor>>(data_.get(), size_))
+
+        template<typename... DimSizes>
+        Tensor(const DimSizes... dims) :
+            Tensor(std::shared_ptr<T>(new T[Eigen::DSizes<Eigen::Index, ndims>(dims...).TotalSize()]),
+                   Eigen::DSizes<Eigen::Index, ndims>(dims...))
         { }
-        
-        Tensor(Tensor<T, ndims>&& other) noexcept :
-            size_(std::move(other.size_)),
-            data_(std::move(other.data_)),
-            tensor_(Eigen::TensorMap<Eigen::Tensor<T, ndims, Eigen::RowMajor>>(data_.get(), size_))
+
+        Tensor(const Eigen::DSizes<Eigen::Index, ndims> size) :
+            Tensor(std::shared_ptr<T>(new T[size.TotalSize()]), size)
+        { }
+
+        Tensor(const Tensor<T, ndims>&& other) noexcept :
+            Tensor(other)
         { }
 
         Tensor(const Tensor<T, ndims>& other) noexcept :
-            size_(other.size_),
-            data_(other.data_),
-            tensor_(Eigen::TensorMap<Eigen::Tensor<T, ndims, Eigen::RowMajor>>(data_.get(), size_))
+            Tensor(other.data_, other.size_)
         { }
 
+        // All other constructors call this constructor eventually. If
+        // possible, use this constructor directly though. 
+        Tensor(std::shared_ptr<T> data, const Eigen::DSizes<Eigen::Index, ndims> size) :
+            size_(size),
+            data_(data),
+            tensor_(Eigen::TensorMap<Eigen::Tensor<T, ndims, Eigen::RowMajor>>(data_.get(), size_))
+        { }               
+        
         ~Tensor() { }
 
         Tensor<T, ndims> operator=(Tensor<T, ndims> rhs) = delete;
@@ -100,7 +83,7 @@ namespace nn {
             return new_tensor;
         }
         
-        Eigen::Index dimension(Eigen::Index dim) const
+        Eigen::Index dimension(const Eigen::Index dim) const
         {
             return tensor_.dimension(dim);
         }
@@ -116,13 +99,13 @@ namespace nn {
         }
 
         template<typename... Indices>
-        inline T& operator()(Indices&& ...indices)
+        inline T& operator()(const Indices ...indices)
         {
             return tensor_(indices...);
         }
 
         template<typename... Indices>
-        inline const T& operator()(Indices&& ...indices) const
+        inline const T& operator()(const Indices ...indices) const
         {
             return tensor_(indices...);
         }
