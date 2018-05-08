254ca7a06d2698dcfb0a8201a399540b70f71277
Sadjad Fouladi <sfouladi@gmail.com>
2018-05-08 14:38:16 -0700

    yolov3.py: Remove extra whitespaces at the end of the lines.

--- scratch/object-dectection/yolov3.py
+++ scratch/object-dectection/yolov3.py
@@ -100,7 +100,7 @@ class DarknetBlock(nn.Module):
 
         self.block_name = block_name
         self.activaction_func = activaction_func
-        
+
         self.conv0 = nn.Conv2d(nFilter1, nFilter2, kernel_size=1, stride=1, padding=0, bias=False)
         self.bn0 = nn.BatchNorm2d(nFilter2)
         self.conv1 = nn.Conv2d(nFilter2, nFilter1, kernel_size=3, stride=1, padding=1, bias=False)
@@ -120,14 +120,14 @@ class DarknetBlock(nn.Module):
         register_b('{}_bn{}_running_mean'.format(self.block_name, 1), self.bn1.running_mean)
         register_b('{}_bn{}_running_var'.format(self.block_name, 1), self.bn1.running_var)
         register_b('{}_bn{}_weight'.format(self.block_name, 1), self.bn1.weight)
-        register_b('{}_bn{}_bias'.format(self.block_name, 1), self.bn1.bias)        
-        
+        register_b('{}_bn{}_bias'.format(self.block_name, 1), self.bn1.bias)
+
     def forward(self, x):
 
         out = self.activaction_func(self.bn0(self.conv0(x)))
         out = self.activaction_func(self.bn1(self.conv1(out)))
 
-        out = out + x         
+        out = out + x
         return out
 
 
@@ -144,7 +144,7 @@ class DarknetConv(nn.Module):
     def register_weights(self, register_p, register_b):
 
         register_p('{}_conv0_weight'.format(self.conv_name), self.conv.weight)
-        
+
         register_b('{}_bn0_running_mean'.format(self.conv_name), self.bn.running_mean)
         register_b('{}_bn0_running_var'.format(self.conv_name), self.bn.running_var)
         register_b('{}_bn0_weight'.format(self.conv_name), self.bn.weight)
@@ -155,14 +155,14 @@ class DarknetConv(nn.Module):
         out = self.activaction_func(self.bn(self.conv(x)))
         return out
 
-    
+
 class YoloBlock(nn.Module):
     def __init__(self, conv_name, nFilter1, nFilter2, size, stride, padding, activaction_func=nn.LeakyReLU(0.1)):
         super(YoloBlock, self).__init__()
 
         self.conv_name = conv_name
         self.activaction_func = activaction_func
-        
+
         self.conv0 = nn.Conv2d(nFilter1, nFilter2, kernel_size=size, stride=stride, padding=padding, bias=False)
         self.bn0 = nn.BatchNorm2d(nFilter2)
 
@@ -174,14 +174,14 @@ class YoloBlock(nn.Module):
         register_b('{}_bn{}_running_var'.format(self.conv_name, 0), self.bn0.running_var)
         register_b('{}_bn{}_weight'.format(self.conv_name, 0), self.bn0.weight)
         register_b('{}_bn{}_bias'.format(self.conv_name, 0), self.bn0.bias)
-        
+
     def forward(self, x):
 
         out = self.activaction_func(self.bn0(self.conv0(x)))
 
         return out
 
-    
+
 class YoloConv(nn.Module):
     def __init__(self, conv_name, nFilter1, nFilter2, activaction_func=lambda x: x):
         super(YoloConv, self).__init__()
@@ -212,15 +212,15 @@ class YoloUpsample(nn.Module):
     def forward(self, x):
         return self.upsample(x)
 
-    
+
 class YoloV3(nn.Module):
-    
+
     darknet53_block_structure = [1, 2, 8, 8, 4]
     yolo_block_structure = [3, 3, 3]
-    
+
     def __init__(self):
         super(YoloV3, self).__init__()
-        
+
         # darknet53 layers (the first 52 conv layers are present)
         self.darknet53_standalone0 = [ DarknetConv('darknet53_standalone0_instance0', 3, 32, stride=1) ]
         self.darknet53_standalone1 = [ DarknetConv('darknet53_standalone1_instance0', 32, 64) ]
@@ -248,14 +248,14 @@ class YoloV3(nn.Module):
                        self.darknet53_block1 + \
                        self.darknet53_standalone3 + \
                        self.darknet53_block2
-        
+
         self.layer61 = self.darknet53_standalone4 + \
                        self.darknet53_block3
 
         self.layer74 = self.darknet53_standalone5 + \
                        self.darknet53_block4
 
-        # yolo detection layers 
+        # yolo detection layers
         # detection 1
         self.layer79 = [
             YoloBlock('yolo_standalone0_instance0', 1024, 512, 1, 1, 0),
@@ -266,9 +266,9 @@ class YoloV3(nn.Module):
         ]
 
         self.layer80 = [ YoloBlock('yolo_standalone5_instance0', 512, 1024, 3, 1, 1) ]
-        self.layer81 = [ YoloConv('yolo_standalone6_instance0', 1024, 255) ] 
+        self.layer81 = [ YoloConv('yolo_standalone6_instance0', 1024, 255) ]
 
-        
+
         # detection 2
         self.layer85 = [
             YoloBlock('yolo_standalone7_instance0', 512, 256, 1, 1, 0),
@@ -292,7 +292,7 @@ class YoloV3(nn.Module):
             YoloBlock('yolo_standalone15_instance0', 256, 128, 1, 1, 0),
             YoloUpsample()
         ]
-        
+
         self.layer104 = [
             YoloBlock('yolo_standalone16_instance0', 384, 128, 1, 1, 0),
             YoloBlock('yolo_standalone17_instance0', 128, 256, 3, 1, 1),
@@ -303,7 +303,7 @@ class YoloV3(nn.Module):
         ]
 
         self.layer105 = [ YoloConv('yolo_standalone22_instance0', 256, 255) ]
-        
+
         # register darknet layers
         for layer in self.layer36 + self.layer61 + self.layer74:
             layer.register_weights(self.register_parameter, self.register_buffer)
@@ -313,7 +313,7 @@ class YoloV3(nn.Module):
                      self.layer85 + self.layer91 + self.layer92 + \
                      self.layer93 + self.layer97 + self.layer104 + \
                      self.layer105:
-            
+
             layer.register_weights(self.register_parameter, self.register_buffer)
 
 
@@ -324,7 +324,7 @@ class YoloV3(nn.Module):
             x = layer(x)
 
         return x
-            
+
 
     @staticmethod
     def process_prediction(prediction, anchors):
@@ -335,7 +335,7 @@ class YoloV3(nn.Module):
         num_classes = 80
 
         assert prediction.size(2) ==  prediction.size(3), 'height and width must be the same'
-        stride = inp_dim // prediction.size(2) 
+        stride = inp_dim // prediction.size(2)
         grid_size = inp_dim // stride
 
         bbox_attrs = 5 + num_classes
@@ -368,9 +368,9 @@ class YoloV3(nn.Module):
         prediction[:,:,5: 5 + num_classes] = torch.sigmoid((prediction[:,:, 5 : 5 + num_classes]))
         prediction[:,:,:4] *= stride
 
-        return prediction        
+        return prediction
 
-    
+
     def forward(self, x):
 
         # get the intermediates from the darknet featurizer
@@ -382,7 +382,7 @@ class YoloV3(nn.Module):
         layer79 = YoloV3.apply_layers(self.layer79, layer74)
         predict0 = YoloV3.apply_layers(self.layer80 + self.layer81, layer79)
 
-        layer85 = YoloV3.apply_layers(self.layer85, layer79)        
+        layer85 = YoloV3.apply_layers(self.layer85, layer79)
         layer86 = torch.cat((layer85, layer61), 1)
 
         layer91 = YoloV3.apply_layers(self.layer91, layer86)
@@ -392,22 +392,22 @@ class YoloV3(nn.Module):
         layer98 = torch.cat((layer97, layer36), 1)
 
         predict2 = YoloV3.apply_layers(self.layer104 + self.layer105, layer98)
-        
+
         # process the detections
         detections0 = YoloV3.process_prediction(predict0, [(116,90), (156,198), (373,326)])
         detections1 = YoloV3.process_prediction(predict1, [(30,61), (62,45), (59,119)])
         detections2 = YoloV3.process_prediction(predict2, [(10,13), (16,30), (33,23)])
-                
+
         return torch.cat((detections0, detections1, detections2), 1)
 
-    
+
 if __name__ == '__main__':
 
     # construct the model
     yolov3 = YoloV3()
-    
+
     # load the weights
-    with h5py.File('yolov3.h5', 'r') as f: 
+    with h5py.File('yolov3.h5', 'r') as f:
 
         model_params = yolov3.state_dict()
         for param_name in model_params.keys():
@@ -415,27 +415,27 @@ if __name__ == '__main__':
             weights = torch.from_numpy(np.asarray(f[param_name]).astype(np.float32))
             model_params[param_name].data.copy_(weights)
 
-            
+
     # load the input image
     img = Image.open('dog.jpg').convert('RGB')
     #img = Image.open('img3.jpg').convert('RGB')
     img = img.resize((416,416))
     img_ = np.asarray(img)
-    img_ = img_.transpose((2,0,1)) # H X W X C -> C X H X W 
-    
+    img_ = img_.transpose((2,0,1)) # H X W X C -> C X H X W
+
     img_ = np.expand_dims(img_, 0)
     img_ = torch.from_numpy(img_).float().div(255.0)
-    
+
     x = Variable(img_)
     print('input', x.shape)
-    
+
     # perform the forward pass
     t1 = timeit.default_timer()
     y = yolov3(x)
     t2 = timeit.default_timer()
     print('inference time:', t2-t1)
 
-    
+
     # print out detections, if any
     for i in range(y.shape[1]):
         coords = y[0,i,:4]
