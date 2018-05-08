b414fb91eb3c643eb309d877189d2b219ab485c1
Sadjad Fouladi <sfouladi@gmail.com>
2018-05-08 16:53:26 -0700

    yolov3.py: Move dumping detections to a separate function.

--- scratch/object-dectection/yolov3.py
+++ scratch/object-dectection/yolov3.py
@@ -326,6 +326,30 @@ def load_model():
 
     return yolov3
 
+def dump_detections(y, img):
+    for i in range(y.shape[1]):
+        coords = y[0, i, :4]
+        objectness = y[0, i, 4]
+        classes = y[0, i, 5:]
+
+        detections = []
+        if objectness > 0.6:
+            confidences = y[0, i, 5:].detach().numpy()
+            confidences /= sum(confidences)
+            idx = np.argmax(confidences)
+
+            print('"' + coco_names[idx] + '"',
+                  '(' + str(confidences[idx]) + ')',
+                  y[0, i, :4].detach().numpy())
+
+            x1 = y[0, i, 0] - y[0, i ,2] / 2
+            x2 = y[0, i, 0] + y[0, i ,2] / 2
+            y1 = y[0, i, 1] - y[0, i ,3] / 2
+            y2 = y[0, i, 1] + y[0, i ,3] / 2
+
+            draw = ImageDraw.Draw(img)
+            draw.rectangle(((x1, y1), (x2, y2)))
+
 def main():
     parser = argparse.ArgumentParser('Run YoloV3 on input image.')
     parser.add_argument('image', nargs='+')
@@ -337,10 +361,9 @@ def main():
         img_original = Image.open(img_path).convert('RGB')
         img_original = img_original.resize((416, 416))
         img = np.asarray(img_original)
-        img = img.transpose((2, 0, 1)) # H X W X C -> C X H X W
+        img = img.transpose((2, 0, 1))
         img = np.expand_dims(img, 0)
         img = torch.from_numpy(img).float().div(255.0)
-
         x = Variable(img)
 
         print('>> %s:' % img_path)
@@ -349,31 +372,8 @@ def main():
         y = yolov3(x)
         print('inference time:', timeit.default_timer() - t1)
 
-        # print out detections, if any
-        for i in range(y.shape[1]):
-            coords = y[0, i, :4]
-            objectness = y[0, i, 4]
-            classes = y[0, i, 5:]
-
-            detections = []
-            if objectness > 0.6:
-                confidences = y[0, i, 5:].detach().numpy()
-                confidences /= sum(confidences)
-                idx = np.argmax(confidences)
-
-                print('"' + coco_names[idx] + '"',
-                      '(' + str(confidences[idx]) + ')',
-                      y[0, i, :4].detach().numpy())
-
-                x1 = y[0, i, 0] - y[0, i ,2] / 2
-                x2 = y[0, i, 0] + y[0, i ,2] / 2
-                y1 = y[0, i, 1] - y[0, i ,3] / 2
-                y2 = y[0, i, 1] + y[0, i ,3] / 2
-                draw = ImageDraw.Draw(img_original)
-                draw.rectangle(((x1, y1), (x2, y2)))
-
+        dump_detections(y, img_original)
         img_original.save(os.path.splitext(img_path)[0] + '.out.jpg')
-
 
 if __name__ == '__main__':
     main()
