641a2182cd6309968a8c9d47c7a7bb28e2e30020
John Emmons <emmons.john@gmail.com>
2018-05-17 14:48:33 -0700

    added code for getting performance numbers

--- scratch/cifar10/training_scripts/simplenet.py
+++ scratch/cifar10/training_scripts/simplenet.py
@@ -45,5 +45,80 @@ class SimpleNet9(nn.Module):
         out = self.linear(out)
         return out
 
+class SimpleNet9_shallow(nn.Module):
+    def __init__(self, num_classes=10):
+        super(SimpleNet9_shallow, self).__init__()
 
+        self.conv0 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn0 = nn.BatchNorm2d(32)
+        self.conv1 = nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(32)
+
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(64)
+
+        self.conv4 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn4 = nn.BatchNorm2d(128)
+        self.conv5 = nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1, bias=False)
+        self.bn5 = nn.BatchNorm2d(128)
+        
+        self.conv6 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn6 = nn.BatchNorm2d(256)
+        self.conv7 = nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=1, bias=False)
+        self.bn7 = nn.BatchNorm2d(256)
+
+        self.linear = nn.Linear(256, num_classes)
+
+    def forward(self, x):
+        out = F.relu(self.bn0(self.conv0(x)))
+        out = F.relu(self.bn1(self.conv1(out)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = F.relu(self.bn3(self.conv3(out)))
+        out = F.relu(self.bn4(self.conv4(out)))
+        out = F.relu(self.bn5(self.conv5(out)))
+        out = F.relu(self.bn6(self.conv6(out)))
+        out = F.relu(self.bn7(self.conv7(out)))
+        
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+
+class SimpleNet7(nn.Module):
+    def __init__(self, num_classes=10):
+        super(SimpleNet9, self).__init__()
+
+        self.conv0 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn0 = nn.BatchNorm2d(64)
+        self.conv1 = nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.conv3 = nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(128)
+
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn4 = nn.BatchNorm2d(256)
+        self.conv5 = nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=1, bias=False)
+        self.bn5 = nn.BatchNorm2d(256)
+        
+        self.linear = nn.Linear(256, num_classes)
+
+    def forward(self, x):
+        out = F.relu(self.bn0(self.conv0(x)))
+        out = F.relu(self.bn1(self.conv1(out)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = F.relu(self.bn3(self.conv3(out)))
+        out = F.relu(self.bn4(self.conv4(out)))
+        out = F.relu(self.bn5(self.conv5(out)))
+        
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+    
 
