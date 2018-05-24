bee6652f974906f68515a6cb5695a9070eeca295
John Emmons <emmons.john@gmail.com>
2018-05-24 10:57:44 -0700

    made the test-environment script more robust

--- tests/test-environment.sh
+++ tests/test-environment.sh
@@ -7,12 +7,12 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(readlink -f $DIR/../src/nnfc/.libs)
 lib_count=$(ls $DIR/../python/build/ | grep 'lib' | wc -l)
 if [[ $lib_count != '1' ]]; then
     echo -e '\n\n'
-    echo "ERROR: too many python lib dirs! (count: $lib_count) (ls: $(ls ../python/build/))"
+    echo "ERROR: too many python lib dirs! (count: $lib_count) (ls: $(ls $DIR/../python/build/))"
     echo -e '\n\n'
     exit -1
 fi
 
-libdir=../python/build/$(ls $DIR/../python/build/ | grep 'lib')
+libdir=$DIR/../python/build/$(ls $DIR/../python/build/ | grep 'lib')
 
 export PYTHONPATH=$(readlink -f $libdir):$PYTHONPATH
 
