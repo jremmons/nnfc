1f6fb412c7821a66ef6916d46fe53d4148ba264b
Sadjad Fouladi <sfouladi@gmail.com>
2018-05-24 17:15:04 -0700

    Enable arithmetic coding for JPEG compression.

--- src/nnfc/jpeg_codec.hh
+++ src/nnfc/jpeg_codec.hh
@@ -1,10 +1,15 @@
 #ifndef _NNFC_JPEG_H
 #define _NNFC_JPEG_H
 
-#include <turbojpeg.h>
-
 #include <cstdint>
 #include <vector>
+#include <functional>
+
+extern "C" {
+  #include <jpeglib.h>
+}
+
+#include <turbojpeg.h>
 
 #include "nnfc_CXXAPI.hh"
 #include "tensor.hh"
@@ -14,24 +19,25 @@ namespace nnfc {
     class JPEGEncoder
     {
     private:
-        const int quantizer_;
-        std::unique_ptr<void, void(*)(void*)> jpeg_compressor;
-        
+        const int quality_;
+        std::vector<uint8_t> compress(std::vector<uint8_t> & buffer,
+                                      const size_t width, const size_t height);
+
     public:
-        JPEGEncoder(int quantizer);
-        ~JPEGEncoder();
+        JPEGEncoder(int quality);
+        ~JPEGEncoder() {}
 
         std::vector<uint8_t> forward(nn::Tensor<float, 3> input);
         nn::Tensor<float, 3> backward(nn::Tensor<float, 3> input);
 
         static nnfc::cxxapi::constructor_type_list initialization_params() { return { {"quantizer", typeid(int)} }; }
     };
-    
+
     class JPEGDecoder
     {
     private:
         std::unique_ptr<void, void(*)(void*)> jpeg_decompressor;
-        
+
     public:
         JPEGDecoder();
         ~JPEGDecoder();
