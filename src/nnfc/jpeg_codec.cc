eda8d513637f9d52c8d48459a78785ed9a7152ba
Sadjad Fouladi <sfouladi@gmail.com>
2018-06-05 14:20:30 -0700

    Move JPEGEnoder to a separate class (WIP).

--- src/nnfc/jpeg_codec.cc
+++ src/nnfc/jpeg_codec.cc
@@ -1,4 +1,4 @@
-#include <turbojpeg.h>
+#include "jpeg_codec.hh"
 
 #include <chrono>
 #include <cassert>
@@ -9,51 +9,11 @@
 #include <cmath>
 
 #include "tensor.hh"
-#include "jpeg_codec.hh"
-
 using namespace std;
 
 nnfc::JPEGEncoder::JPEGEncoder(int quality)
-    : quality_(quality)
+    : encoder_(quality)
 { }
-
-vector<uint8_t> nnfc::JPEGEncoder::compress(vector<uint8_t> & buffer,
-                                            const size_t width,
-                                            const size_t height)
-{
-    jpeg_compress_struct context;
-    jpeg_error_mgr jerr;
-
-    jpeg_create_compress(&context);
-    context.in_color_space = JCS_GRAYSCALE;
-    jpeg_set_defaults(&context);
-    jpeg_set_quality(&context, quality_, true);
-    context.arith_code = true;
-    context.err = jpeg_std_error(&jerr);
-    context.dct_method = JDCT_FASTEST;
-
-    long unsigned int jpeg_size = 0;
-    unsigned char * compressed_image = nullptr;
-
-    context.image_width = width;
-    context.image_height = height;
-    context.input_components = 1;
-
-    jpeg_mem_dest(&context, &compressed_image, &jpeg_size);
-    jpeg_start_compress(&context, true);
-
-    while (context.next_scanline < context.image_height) {
-        unsigned char * location = &buffer[context.next_scanline * width];
-        jpeg_write_scanlines(&context, &location, 1);
-    }
-
-    jpeg_finish_compress(&context);
-    jpeg_destroy_compress(&context);
-
-    vector<uint8_t> result {compressed_image, compressed_image + jpeg_size};
-    free(compressed_image);
-    return result;
-}
 
 vector<uint8_t> nnfc::JPEGEncoder::forward(nn::Tensor<float, 3> input)
 {
@@ -97,7 +57,7 @@ vector<uint8_t> nnfc::JPEGEncoder::forward(nn::Tensor<float, 3> input)
     }
 
     // JPEG compress
-    vector<uint8_t> encoding = compress(buffer, jpeg_width, jpeg_height);
+    vector<uint8_t> encoding = encoder_.encode(buffer, jpeg_width, jpeg_height, 1);
 
     const uint8_t *min_bytes = reinterpret_cast<const uint8_t *>(&min);
     const uint8_t *max_bytes = reinterpret_cast<const uint8_t *>(&max);
