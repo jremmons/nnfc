1b8d15ccb37c3fee052933a45c0b9d53e53b84aa
John Emmons <emmons.john@gmail.com>
2018-06-06 14:01:09 -0700

    added mean image subtraction

--- scratch/object-detection/utils.py
+++ scratch/object-detection/utils.py
@@ -86,14 +86,19 @@ def iou(box_1, box_2):
 
     return f_area(box_i) / (f_area(box_1) + f_area(box_2) - f_area(box_i))
 
-def normalize_image(img_path, size=416):
-    img_original = Image.open(img_path).convert('RGB')
-    img_original = img_original.resize((size, size))
-    img = np.asarray(img_original)
-    img = img.transpose((2, 0, 1))
-    img = np.expand_dims(img, 0)
-    img = torch.from_numpy(img).float().div(255.0)
-    return img
+# def normalize_image(img_path, size=416):
+
+#     # TODO(jremmons) add image mean substraction to this function.
+#     # TODO(jremmons) add any other data augmentation that yolo might do. 
+
+#     img_original = Image.open(img_path).convert('RGB')
+#     img_original = img_original.resize((size, size))
+#     img = np.asarray(img_original)
+#     img = img.transpose((2, 0, 1))
+    
+#     img = np.expand_dims(img, 0)
+#     img = torch.from_numpy(img).float().div(255.0)
+#     return img
 
 def non_max_suppression(detections, confidence_threshold=0.25, iou_threshold=0.4):
     # throw out the results with confidence less than the threshold
