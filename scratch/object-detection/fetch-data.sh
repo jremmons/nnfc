8eb39137b9157f385cfa79a7292d53b3af537246
Sadjad Fouladi <sfouladi@gmail.com>
2018-06-06 02:47:18 -0700

    fetch-data.sh: Download labels for mscoco.

--- scratch/object-detection/fetch-data.sh
+++ scratch/object-detection/fetch-data.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-mkdir coco
-cd coco
-
+mkdir -p coco/images
+cd coco/images
 wget -c https://pjreddie.com/media/files/val2014.zip
 unzip -q val2014.zip
+
+cd ..
+wget -c https://pjreddie.com/media/files/coco/labels.tgz
+tar xzf labels.tgz
