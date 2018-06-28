64f832da24787a829c4a38ae27ac79b2386d9aa2
John Emmons <emmons.john@gmail.com>
2018-06-27 17:31:10 -0700

    added more work on the autoencoder

---
+++ scratch/object-detection/train_transform/utils.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+
+import torch
+import numpy as np
+from PIL import Image
+
+coco_names = [
+    'person',        'bicycle',       'car',           'motorbike',
+    'aeroplane',     'bus',           'train',         'truck',
+    'boat',          'traffic light', 'fire hydrant',  'stop sign',
+    'parking meter', 'bench',         'bird',          'cat',
+    'dog',           'horse',         'sheep',         'cow',
+    'elephant',      'bear',          'zebra',         'giraffe',
+    'backpack',      'umbrella',      'handbag',       'tie',
+    'suitcase',      'frisbee',       'skis',          'snowboard',
+    'sports ball',   'kite',          'baseball bat',  'baseball glove',
+    'skateboard',    'surfboard',     'tennis racket', 'bottle',
+    'wine glass',    'cup',           'fork',          'knife',
+    'spoon',         'bowl',          'banana',        'apple',
+    'sandwich',      'orange',        'broccoli',      'carrot',
+    'hot dog',       'pizza',         'donut',         'cake',
+    'chair',         'sofa',          'pottedplant',   'bed',
+    'diningtable',   'toilet',        'tvmonitor',     'laptop',
+    'mouse',         'remote',        'keyboard',      'cell phone',
+    'microwave',     'oven',          'toaster',       'sink',
+    'refrigerator',  'book',          'clock',         'vase',
+    'scissors',      'teddy bear',    'hair drier',    'toothbrush',
+]
+
+class DetectionOutput:
+    def __init__(self):
+        self.coco_idx = None
+        self.coco_name = None
+        self.confidence = None
+        self.bb = None
+
+    def __str__(self):
+        return '"' + self.coco_name + '" (' + \
+               str(self.confidence) + ') ' + str(self.bb)
+
+    def __repr__(self):
+        return str(self)
+
+def parse_detections(y):
+
+    object_threshold = 0.2
+    y = y.detach().cpu().numpy()
+    
+    all_detections = []
+    for k in range(y.shape[0]):
+
+        detections = []
+        detection_indices = np.argwhere(y[k, :, 4] > object_threshold)
+        
+        for i in detection_indices:
+            confidences = y[k, i, 5:][0]
+            confidences /= sum(confidences)
+            idx = np.argmax(confidences)
+            
+            det = DetectionOutput()
+            det.coco_idx = idx
+            det.coco_name = coco_names[idx]
+            det.confidence = confidences[idx]
+
+            bb = y[k, i, :4][0]
+            det.bb = [
+                bb[0] - bb[2] / 2, #x1
+                bb[1] - bb[3] / 2, #y1
+                bb[0] + bb[2] / 2, #x2
+                bb[1] + bb[3] / 2  #y2
+            ]
+
+            detections += [det]
+
+        all_detections += [detections]
+
+    return all_detections
+
+# box = [x1, y1, x2, y2]
+def iou(box_1, box_2):
+    f_area = lambda b: ((b[3] - b[1] + 1) * (b[2] - b[0] + 1))
+
+    # intersection coordinates
+    box_i = [max(box_1[0], box_2[0]), max(box_1[1], box_2[1]),
+             min(box_1[2], box_2[2]), min(box_1[3], box_2[3])]
+
+    return f_area(box_i) / (f_area(box_1) + f_area(box_2) - f_area(box_i))
+
+def non_max_suppression(detections, confidence_threshold=0.25, iou_threshold=0.5):
+    # throw out the results with confidence less than the threshold
+    outputs = [det for det in detections if det.confidence >= confidence_threshold]
+
+    if not outputs:
+        return outputs
+
+    final_outputs = []
+    for label in {det.coco_idx for det in outputs}:
+        label_outs = [det for det in outputs if det.coco_idx == label]
+        label_outs.sort(key=lambda x: x.confidence, reverse=True)
+
+        label_finals = []
+        while label_outs:
+            label_finals += [label_outs[0]]
+            del label_outs[0]
+            label_outs = [det for det in label_outs
+                          if iou(label_finals[-1].bb, det.bb) < iou_threshold]
+
+        final_outputs += label_finals
+
+    return final_outputs
---
+++ scratch/object-detection/train_transform/yolov3_ae.py
@@ -0,0 +1,101 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch.autograd import Variable
+
+
+class AutoEncoderI(nn.Module):
+
+    def __init__(self):
+        super(AutoEncoderI, self).__init__()
+        self.conv1 = nn.Conv2d(256, 256, kernel_size=1, stride=1, padding=0, bias=False)
+        #self.w1 = nn.Parameter(torch.randn(1))
+        #self.register_parameter('weight', self.w1)
+        
+    def forward(self, x):
+        out = x
+        #out = self.w1 + out
+        out = self.conv1(out)
+
+        return out
+
+class AutoEncoder0(nn.Module):
+
+    def __init__(self):
+        super(AutoEncoder0, self).__init__()
+        stride = 1
+
+        # encoder
+        self.conv1 = nn.Conv2d(256, 256, kernel_size=1, stride=1, padding=0, bias=False)
+        self.conv_bn1 = nn.BatchNorm2d(256)
+
+        self.conv3 = nn.Conv2d(256, 64, kernel_size=1, stride=1, padding=0, bias=False)
+        self.conv_bn3 = nn.BatchNorm2d(64)
+
+
+        # decoder
+        self.deconv1 = nn.ConvTranspose2d(64, 64, kernel_size=1, stride=1, padding=0, bias=False)
+        self.deconv_bn1 = nn.BatchNorm2d(64)
+
+        self.deconv3 = nn.ConvTranspose2d(64, 256, kernel_size=1, stride=1, padding=0, bias=False)
+        self.deconv_bn3 = nn.BatchNorm2d(256)
+
+        
+    def forward(self, x):
+        out = x
+        out = F.relu(self.conv_bn1(self.conv1(out)))
+        out = F.relu(self.conv_bn3(self.conv3(out)))
+
+        out = F.relu(self.deconv_bn1(self.deconv1(out)))
+        out = F.relu(self.deconv_bn3(self.deconv3(out)))
+        
+        return out
+
+class AutoEncoder1(nn.Module):
+
+    def __init__(self):
+        super(AutoEncoder1, self).__init__()
+        stride = 1
+
+        # encoder
+        self.conv1 = nn.Conv2d(256, 256, kernel_size=1, stride=1, padding=0, bias=False)
+        self.conv_bn1 = nn.BatchNorm2d(256)
+
+        self.conv2 = nn.Conv2d(256, 128, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv_bn2 = nn.BatchNorm2d(128)
+        
+        self.conv3 = nn.Conv2d(128, 128, kernel_size=1, stride=1, padding=0, bias=False)
+        self.conv_bn3 = nn.BatchNorm2d(128)
+
+        self.conv4 = nn.Conv2d(128, 64, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv_bn4 = nn.BatchNorm2d(64)
+
+
+        # decoder
+        self.deconv1 = nn.ConvTranspose2d(64, 64, kernel_size=1, stride=1, padding=0, bias=False)
+        self.deconv_bn1 = nn.BatchNorm2d(64)
+
+        self.deconv2 = nn.ConvTranspose2d(64, 128, kernel_size=3, stride=1, padding=1, bias=False)
+        self.deconv_bn2 = nn.BatchNorm2d(128)
+
+        self.deconv3 = nn.ConvTranspose2d(128, 128, kernel_size=1, stride=1, padding=0, bias=False)
+        self.deconv_bn3 = nn.BatchNorm2d(128)
+
+        self.deconv4 = nn.ConvTranspose2d(128, 256, kernel_size=3, stride=1, padding=1, bias=False)
+        self.deconv_bn4 = nn.BatchNorm2d(256)
+
+        
+    def forward(self, x):
+        out = x
+        out = F.relu(self.conv_bn1(self.conv1(out)))
+        out = F.relu(self.conv_bn2(self.conv2(out)))
+        out = F.relu(self.conv_bn3(self.conv3(out)))
+        out = F.relu(self.conv_bn4(self.conv4(out)))
+
+        out = F.relu(self.deconv_bn1(self.deconv1(out)))
+        out = F.relu(self.deconv_bn2(self.deconv2(out)))
+        out = F.relu(self.deconv_bn3(self.deconv3(out)))
+        out = F.relu(self.deconv_bn4(self.deconv4(out)))
+        
+        return out
