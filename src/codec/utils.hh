ccb4475b99e128eec938b94eb1f2977f567fabac
Sadjad Fouladi <sfouladi@gmail.com>
2018-06-28 17:15:29 -0700

    Create functions to compute DCT and IDCT.

---
+++ src/codec/utils.hh
@@ -0,0 +1,13 @@
+#ifndef _CODEC_UTILS_HH
+#define _CODEC_UTILS_HH
+
+#include "nn/tensor.hh"
+
+namespace codec::utils {
+
+  nn::Tensor<float, 3> dct(nn::Tensor<float, 3> & input, const int N = 8);
+  nn::Tensor<float, 3> idct(nn::Tensor<float, 3> & input, const int N = 8);
+
+}
+
+#endif /* _CODEC_UTILS_HH */
