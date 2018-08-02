38b27c099de628d0dbce38355304956585379da0
John Emmons <emmons.john@gmail.com>
2018-08-02 13:33:37 -0700

    ran code formatter

--- src/codec/arithmetic_encoder.hh
+++ src/codec/arithmetic_encoder.hh
@@ -6,9 +6,8 @@
 
 namespace codec {
 
-    std::vector<char> arith_encode(std::vector<char> input);
-    std::vector<char> arith_decode(std::vector<char> input);
-    
+std::vector<char> arith_encode(std::vector<char> input);
+std::vector<char> arith_decode(std::vector<char> input);
 }
 
 #endif  // _CODEC_ARITHMETIC_ENCODER_HH
--- src/frontend/arithcode/decode.cc
+++ src/frontend/arithcode/decode.cc
@@ -1,24 +1,25 @@
 #include <iostream>
 
-#include <string>
 #include <fstream>
 #include <streambuf>
+#include <string>
 
 #include "arithmetic_encoder.hh"
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[]) {
+  if (argc != 3) {
+    std::cout << "usage: " << argv[0] << " <input_file> <output_file>\n";
+    return -1;
+  }
 
-    if (argc != 3) {
-        std::cout << "usage: " << argv[0] << " <input_file> <output_file>\n"; 
-        return -1;
-    }
+  std::ifstream input_file(argv[1], std::ios::in | std::ios::binary);
+  std::vector<char> compressed_input(std::istreambuf_iterator<char>{input_file},
+                                     {});
 
-    std::ifstream input_file(argv[1], std::ios::in | std::ios::binary);
-    std::vector<char> compressed_input(std::istreambuf_iterator<char>{input_file}, {});
+  const std::vector<char> uncompressed_output =
+      codec::arith_decode(compressed_input);
+  std::cout << "uncompressed size: " << uncompressed_output.size() << std::endl;
 
-    const std::vector<char> uncompressed_output = codec::arith_decode(compressed_input);
-    std::cout << "uncompressed size: " << uncompressed_output.size() << std::endl;
-    
-    std::ofstream output_file(argv[2], std::ios::out | std::ios::binary);
-    output_file.write(uncompressed_output.data(), uncompressed_output.size());
+  std::ofstream output_file(argv[2], std::ios::out | std::ios::binary);
+  output_file.write(uncompressed_output.data(), uncompressed_output.size());
 }
