89036d632318f4214b5709f5e00a41edd09f7661
John Emmons <emmons.john@gmail.com>
2018-08-14 10:53:50 -0700

    cleaned arithmetic_probability_models.hh

--- src/codec/arithmetic_probability_models.hh
+++ src/codec/arithmetic_probability_models.hh
@@ -7,7 +7,6 @@
 
 namespace codec {
 
-
 class SimpleModel {
  private:
   const std::vector<std::pair<uint32_t, uint32_t>> numerator;
@@ -30,30 +29,6 @@ class SimpleModel {
 
   inline uint32_t size() { return 3; }
 };
-
-    
-// class SimpleAdaptiveModel {
-//  private:
-//   std::vector<std::pair<uint32_t, uint32_t>> numerator;
-//   uint32_t denominator;
-
-//  public:
-//   SimpleModel()
-//       : numerator({
-//             {0, 11000}, {11000, 11999}, {11999, 12000},
-//         }),
-//         denominator(numerator[numerator.size() - 1].second) {}
-//   ~SimpleModel() {}
-
-//   inline std::pair<uint32_t, uint32_t> symbol_numerator(uint32_t symbol) const {
-//     assert(symbol <= 2);
-//     return numerator[symbol];
-//   }
-
-//   inline uint32_t symbol_denominator() const { return denominator; }
-
-//   inline uint32_t size() { return 3; }
-// };
 
 }
 
