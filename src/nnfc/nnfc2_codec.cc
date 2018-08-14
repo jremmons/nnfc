f5a3b37a098fb4e3fe43cacbae6f7e0bf0a3777a
John Emmons <emmons.john@gmail.com>
2018-08-13 20:02:55 -0700

    serialize data in zigzag order

--- src/nnfc/nnfc2_codec.cc
+++ src/nnfc/nnfc2_codec.cc
@@ -68,45 +68,50 @@ std::vector<uint8_t> nnfc::NNFC2Encoder::forward(const nn::Tensor<float, 3> t_in
   //     (q_input.cast<float>() * ((max - min) / 255)) + min;
   // nn::Tensor<float, 3> input(input_t);
 
-  nn::Tensor<float, 3> input(std::move(codec::utils::dct(t_input, BLOCK_WIDTH)));
+  // nn::Tensor<float, 3> input(std::move(codec::utils::dct(t_input, BLOCK_WIDTH)));
+  nn::Tensor<float, 3> input(t_input);
 
-  for (size_t channel = 0; channel < dim0; channel++) {
-      for (size_t row = 0; row < dim1 / BLOCK_WIDTH; row++) {
-          for (size_t col = 0; col < dim2 / BLOCK_WIDTH; col++) {
-              
-              for (size_t i = 0; i < ZIGZAG_LENGTH; i++) {
-                  const size_t row_offset = BLOCK_WIDTH*row + ZIGZAG_ORDER[i][0];
-                  const size_t col_offset = BLOCK_WIDTH*col + ZIGZAG_ORDER[i][1];
-
-                  // check if (value / quantizer) > 0, if not encode EOB and break
-                  // otherwise encode the symbol as normal
-                  input(channel, row_offset, col_offset); 
-              }
-          }
-      }
-  }
-  
-  
   // const float dct_min = dct_input.minimum();
   // const float dct_max = dct_input.maximum();
   // Eigen::Tensor<uint8_t, 3, Eigen::RowMajor> q_input = ((255 * (dct_input.tensor() - dct_min)) / (quantizer_ * dct_max)).cast<uint8_t>();
 
-  // arithmetic encode
-  //  and serialize data
-  
-  for (size_t i = 0; i < dim0; i++) {
-    for (size_t j = 0; j < dim1; j++) {
-      for (size_t k = 0; k < dim2; k++) {
-        float element = input(i, j, k);
-        uint8_t *bytes = reinterpret_cast<uint8_t *>(&element);
-
-        encoding.push_back(bytes[0]);
-        encoding.push_back(bytes[1]);
-        encoding.push_back(bytes[2]);
-        encoding.push_back(bytes[3]);
+  // arithmetic encode and serialize data
+  for (size_t channel = 0; channel < dim0; channel++) {
+      for (size_t block_row = 0; block_row < dim1 / BLOCK_WIDTH; block_row++) {
+          for (size_t block_col = 0; block_col < dim2 / BLOCK_WIDTH; block_col++) {
+              
+              for (size_t i = 0; i < ZIGZAG_LENGTH; i++) {
+                  const size_t row_offset = BLOCK_WIDTH*block_row + ZIGZAG_ORDER[i][0];
+                  const size_t col_offset = BLOCK_WIDTH*block_col + ZIGZAG_ORDER[i][1];
+                  
+                  // check if (value / quantizer) > 0, if not encode EOB and break
+                  // otherwise encode the symbol as normal
+                  float element = input(channel, row_offset, col_offset);
+                  uint8_t *bytes = reinterpret_cast<uint8_t *>(&element);
+                  
+                  encoding.push_back(bytes[0]);
+                  encoding.push_back(bytes[1]);
+                  encoding.push_back(bytes[2]);
+                  encoding.push_back(bytes[3]);
+                  
+              }
+          }
       }
-    }
   }
+    
+  // for (size_t i = 0; i < dim0; i++) {
+  //   for (size_t j = 0; j < dim1; j++) {
+  //     for (size_t k = 0; k < dim2; k++) {
+  //       float element = input(i, j, k);
+  //       uint8_t *bytes = reinterpret_cast<uint8_t *>(&element);
+
+  //       encoding.push_back(bytes[0]);
+  //       encoding.push_back(bytes[1]);
+  //       encoding.push_back(bytes[2]);
+  //       encoding.push_back(bytes[3]);
+  //     }
+  //   }
+  // }
 
   return encoding;
 }
@@ -144,26 +149,50 @@ nn::Tensor<float, 3> nnfc::NNFC2Decoder::forward(const std::vector<uint8_t> inpu
 
   nn::Tensor<float, 3> f_output(dim0, dim1, dim2);
 
-  for (size_t i = 0; i < dim0; i++) {
-    for (size_t j = 0; j < dim1; j++) {
-      for (size_t k = 0; k < dim2; k++) {
-        float element;
-        uint8_t *bytes = reinterpret_cast<uint8_t *>(&element);
-
-        size_t offset = sizeof(float)*(dim1 * dim2 * i + dim2 * j + k) + 3*sizeof(uint64_t);
-        bytes[0] = input[offset];
-        bytes[1] = input[offset + 1];
-        bytes[2] = input[offset + 2];
-        bytes[3] = input[offset + 3];
-
-        f_output(i, j, k) = element;
+  for (size_t channel = 0; channel < dim0; channel++) {
+      for (size_t block_row = 0; block_row < dim1 / BLOCK_WIDTH; block_row++) {
+          for (size_t block_col = 0; block_col < dim2 / BLOCK_WIDTH; block_col++) {
+              
+              for (size_t i = 0; i < ZIGZAG_LENGTH; i++) {
+                  const size_t row_offset = BLOCK_WIDTH*block_row + ZIGZAG_ORDER[i][0];
+                  const size_t col_offset = BLOCK_WIDTH*block_col + ZIGZAG_ORDER[i][1];
+                  
+                  float element;
+                  uint8_t *bytes = reinterpret_cast<uint8_t *>(&element);
+                  
+                  size_t offset = sizeof(float)*(dim1 * dim2 * channel + dim2 * row_offset + col_offset) + 3*sizeof(uint64_t);
+                  bytes[0] = input[offset];
+                  bytes[1] = input[offset + 1];
+                  bytes[2] = input[offset + 2];
+                  bytes[3] = input[offset + 3];
+                  
+                  f_output(channel, row_offset, col_offset) = element;
+              }
+          }
       }
-    }
   }
+  
+  // for (size_t i = 0; i < dim0; i++) {
+  //   for (size_t j = 0; j < dim1; j++) {
+  //     for (size_t k = 0; k < dim2; k++) {
+  //       float element;
+  //       uint8_t *bytes = reinterpret_cast<uint8_t *>(&element);
+
+  //       size_t offset = sizeof(float)*(dim1 * dim2 * i + dim2 * j + k) + 3*sizeof(uint64_t);
+  //       bytes[0] = input[offset];
+  //       bytes[1] = input[offset + 1];
+  //       bytes[2] = input[offset + 2];
+  //       bytes[3] = input[offset + 3];
+
+  //       f_output(i, j, k) = element;
+  //     }
+  //   }
+  // }
 
   
-  nn::Tensor<float, 3> output(std::move(codec::utils::idct(f_output, BLOCK_WIDTH)));
-
+  // nn::Tensor<float, 3> output(std::move(codec::utils::idct(f_output, BLOCK_WIDTH)));
+  nn::Tensor<float, 3> output(f_output);
+  
   return output;
 }
 
