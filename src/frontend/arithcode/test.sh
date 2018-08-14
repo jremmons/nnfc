4567037b2ee372c4e9c5af070c4eeda652af7559
John Emmons <emmons.john@gmail.com>
2018-08-14 12:05:22 -0700

    encodes and decoders text.small.txt now

--- src/frontend/arithcode/test.sh
+++ src/frontend/arithcode/test.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-./arith_encode example-text/text.txt compressed.bin
+./arith_encode example-text/text.small.txt compressed.bin
 
 rm -f output.txt
 ./arith_decode compressed.bin output.txt
 
-diff -q example-text/text.txt output.txt
+diff -q example-text/text.small.txt output.txt
 if [ $? != 0 ]; then
     echo "failure!"
 else
